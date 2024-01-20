a8691963121a925513cba0a862761f92713d3920
pgrela <pgrela@>
2024-01-20 14:05:44 +0100

    [php] one big form in html

--- src/main/php/index.php
+++ src/main/php/index.php
@@ -4,28 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vegetable Voting</title>
-    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="styles.css"/>
 </head>
 <body>
 
 <h1>Vegetable Voting</h1>
 
-<div class="grid-container">
-    <?php for($i = 1; $i <= 30; $i++): ?>
-    <div class="vegetable">
-        <div class="vegetable-image"><img src="./img/vegetable-<?php echo $i; ?>.png" alt="Carrot"></div>
-        <div class="buttons-bar">
-            <div class="like-wrapper">
-                <button class="like">Like 👍</button>
+<?php
+    if(!empty($_POST))var_dump($_POST);
+?>
+
+<form action="index.php" method="post">
+    <div class="grid-container">
+        <?php for ($i = 1; $i <= 30; $i++): ?>
+            <div class="vegetable">
+                <div class="vegetable-image"><img src="./img/vegetable-<?php echo $i; ?>.png" alt="Carrot"></div>
+                <div class="buttons-bar">
+                    <div class="like-wrapper">
+                        <input type="submit" name="vegetable-like-<?php echo $i; ?>" class="like" value="Like 👍" />
+                    </div>
+                    <div class="likes">Total Likes: <span class="likes-number">0</span></div>
+                    <div class="dislike-wrapper">
+                        <input type="submit" name="vegetable-like-<?php echo $i; ?>" class="dislike" value="Dislike 👎" />
+                    </div>
+                </div>
             </div>
-            <div class="likes">Total Likes: <span class="likes-number">0</span></div>
-            <div class="dislike-wrapper">
-                <button class="dislike">Dislike 👎</button>
-            </div>
-        </div>
+        <?php endfor; ?>
     </div>
-    <?php endfor; ?>
-</div>
+</form>
 
 </body>
 </html>
